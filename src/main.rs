28e1b87770c8f8bb089bc40cd2e14f94e6325bd0
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-09-02 00:18:44 +0700

    generate secret num from 1 to 100

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
 
     println!("Welcome to the Guessing Game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Input your guess");
 
     let mut guess_number = String::new();
