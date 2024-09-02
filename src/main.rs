4c4682608723d477839d94c8da667ab778f90adf
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-09-02 18:06:32 +0700

    add infinite loop for user to guess

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,15 @@ fn main() {
     let guess_number: u32 = guess_number.trim().parse().unwrap();
 
     println!("Your guess number is: {}", guess_number);
+    
+    // infinite loop
+    loop {
 
-    match guess_number.cmp(&secret_number) {
-        Ordering::Less => println!("Wrong! Seems like you should try a bigger number!"),
-        Ordering::Equal => println!("Congratulations! You guess the correct number!"),
-        Ordering::Greater => println!("Wrong! Maybe try a smaller number?")
+        match guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("Wrong! Seems like you should try a bigger number!"),
+            Ordering::Equal => println!("Congratulations! You guess the correct number!"),
+            Ordering::Greater => println!("Wrong! Maybe try a smaller number?")
+        }
+
     }
 }
