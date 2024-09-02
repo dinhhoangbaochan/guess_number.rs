12ad69985b324ef3d5d6b232126785de2e4f3821
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-09-02 16:03:42 +0700

    add comparision & convert guess number type

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
@@ -14,5 +14,16 @@ fn main() {
 
     io::stdin().read_line(&mut guess_number).expect("Failed to get guess number!");
 
+    // this one is called `variable shadowing`
+    // it lets us reuse the previous variable
+    // guess_number was a string, need to convert to i32, an int with 32-bit, same as secret_number, so we can compare.
+    let guess_number: u32 = guess_number.trim().parse().unwrap();
+
     println!("Your guess number is: {}", guess_number);
+
+    match guess_number.cmp(&secret_number) {
+        Ordering::Less => println!("Wrong! Seems like you should try a bigger number!"),
+        Ordering::Equal => println!("Congratulations! You guess the correct number!"),
+        Ordering::Greater => println!("Wrong! Maybe try a smaller number?")
+    }
 }
